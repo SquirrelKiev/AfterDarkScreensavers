bddcf7f24700117bf11b92585d12ede701026029
Ethan Ginns-Farrow <laidbackenon@outlook.com>
2022-04-09 12:16:19 +0100

    Improved toaster position deciding code

--- src/FlyingToasters/ToasterController.cs
+++ src/FlyingToasters/ToasterController.cs
@@ -19,9 +19,23 @@ namespace AfterDarkScreensavers.FlyingToasters
         {
             if (lastToasterDepartureTime + timeBetweenToasters <= Raylib.GetTime())
             {
-                // TODO: Fix to spawn respectably (appear on screen when they become real)
-                int toasterY = random.Next(0 - Raylib.GetScreenHeight() / 2, Raylib.GetScreenHeight() / 2);
-                int toasterX = (Raylib.GetScreenWidth() + 10);
+                double toastPosDecider = random.NextDouble() * 2;
+
+                int toasterX;
+                int toasterY;
+
+                if (toastPosDecider <= 1)
+                {
+                    toasterX = (int)(Raylib.GetScreenWidth() / 2 * toastPosDecider) + Raylib.GetScreenWidth() / 2;
+                    toasterY = -10;
+                }
+                else
+                {
+                    toasterX = Raylib.GetScreenWidth() + 10;
+                    toasterY = (int)(Raylib.GetScreenHeight() / 2 * (toastPosDecider - 1));
+                }
+
+                
 
                 Console.WriteLine($"Toaster departed at {Raylib.GetTime()} X: {toasterX} Y: {toasterY}");
 
@@ -30,7 +44,9 @@ namespace AfterDarkScreensavers.FlyingToasters
                 lastToasterDepartureTime = Raylib.GetTime();
             }
 
-            List<Toaster> toastersToRemove = new List<Toaster>();
+            List<Toaster> toastersToRemove = null;
+
+            Raylib.DrawText($"{Math.Round(Raylib.GetTime() - lastToasterDepartureTime, 3)}", 10, 10, 36, Color.GREEN);
 
             foreach (Toaster toaster in toasters)
             {
@@ -42,13 +58,19 @@ namespace AfterDarkScreensavers.FlyingToasters
                 if (toaster.position.X <= -25 || toaster.position.Y >= Raylib.GetScreenHeight())
                 {
                     Console.WriteLine($"Toaster sent to landfill at {Raylib.GetTime()}");
+                    if(toastersToRemove == null)
+                        toastersToRemove = new List<Toaster>();
+
                     toastersToRemove.Add(toaster);
                 }
             }
 
-            foreach(Toaster toaster in toastersToRemove)
+            if(toastersToRemove != null)
             {
-                toasters.Remove(toaster);
+                foreach (Toaster toaster in toastersToRemove)
+                {
+                    toasters.Remove(toaster);
+                }
             }
         }
     }
